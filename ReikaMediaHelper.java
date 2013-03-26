f861b36928aed496f4b9a931bfc7722387d427bd
Reika <reikasminecraft@gmail.com>
2013-03-26 01:33:54 -0400

    Added a method

--- ReikaMediaHelper.java
+++ ReikaMediaHelper.java
@@ -1,5 +1,15 @@
 package Reika.DragonAPI;
 
-public class ReikaMediaHelper {
+import java.util.Random;
 
+import net.minecraft.world.World;
+
+public class ReikaMediaHelper {
+	
+	static Random par5Random = new Random();
+	
+	public static void playSoundVarying(World world, int x, int y, int z, String name) {
+		world.playSoundEffect(x+0.5, y+0.5, z+0.5, name, par5Random.nextFloat(), par5Random.nextFloat());
+	}
+	
 }
