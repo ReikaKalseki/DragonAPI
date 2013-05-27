6f49f5ff149a85616e7f23515dc15195d7cb92a4
Reika <reikasminecraft@gmail.com>
2013-05-27 14:26:34 -0400

    licensing

--- DragonAPICore.java
+++ DragonAPICore.java
@@ -2,6 +2,17 @@ package Reika.DragonAPI;
 
 import java.util.Random;
 
+/** @author Reika
+*
+* This code as part of the API "DragonAPI" for the game "Minecraft" are the property of and owned by Reika
+*
+* Any unauthorized distribution, modification, and especially sale of this code is a full violation of copyright law and will be
+* dealt with accordingly.
+*
+* This restriction and these copyrights apply not only to this class file, but also to every other DragonAPI class.
+*
+*/
+
 public class DragonAPICore {
 
 	public DragonAPICore() {throw new RuntimeException("The class "+this.getClass()+" cannot be instantiated!");}
