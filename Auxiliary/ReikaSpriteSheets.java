0c77d0ec6ca5a8912d9e344cb3737f4e090ff1e8
Reika <reikasminecraft@gmail.com>
2013-05-28 16:04:06 -0400

    Moved base folder - Updated to 1.5.2
    Also, NOW the final license change.

--- Auxiliary/ReikaSpriteSheets.java
+++ Auxiliary/ReikaSpriteSheets.java
@@ -1,28 +1,11 @@
 /*******************************************************************************
  * @author Reika
  * 
- * This code is the property of and owned and copyrighted by Reika.
- * This code is provided under a modified visible-source license that is as follows:
+ * Copyright 2013
  * 
- * Any and all users are permitted to use the source for educational purposes, or to create other mods that call
- * parts of this code and use DragonAPI as a dependency.
- * 
- * Unless given explicit written permission - electronic writing is acceptable - no user may redistribute this
- * source code nor any derivative works. These pre-approved works must prominently contain this copyright notice.
- * 
- * Additionally, no attempt may be made to achieve monetary gain from this code by anyone except the original author.
- * In the case of pre-approved derivative works, any monetary gains made will be shared between the original author
- * and the other developer(s), proportional to the ratio of derived to original code.
- * 
- * Finally, any and all displays, duplicates or derivatives of this code must be prominently marked as such, and must
- * contain attribution to the original author, including a link to the original source. Any attempts to claim credit
- * for this code will be treated as intentional theft.
- * 
- * Due to the Mojang and Minecraft Mod Terms of Service and Licensing Restrictions, compiled versions of this code
- * must be provided for free. However, with the exception of pre-approved derivative works, only the original author
- * may distribute compiled binary versions of this code.
- * 
- * Failure to comply with these restrictions is a violation of copyright law and will be dealt with accordingly.
+ * All rights reserved.
+ * Distribution of the software in any form is only allowed with
+ * explicit, prior permission from the owner.
  ******************************************************************************/
 package Reika.DragonAPI.Auxiliary;
 
@@ -35,7 +18,7 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 
 public final class ReikaSpriteSheets {
-	
+
 	private ReikaSpriteSheets() {throw new RuntimeException("The class "+this.getClass()+" cannot be instantiated!");}
 
 	/** Call this from a registered ItemRenderer class that implements IItemRenderer to actually render the item.
@@ -45,18 +28,18 @@ public final class ReikaSpriteSheets {
 		if (item == null)
 			return;
 		int row = index/16;
-		int col = index-row*16;		
+		int col = index-row*16;
 		//ModLoader.getMinecraftInstance().entityRenderer.disableLightmap(1);
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, tex);
 		if (type == type.INVENTORY)
 			GL11.glDisable(GL11.GL_LIGHTING);
-        GL11.glPushMatrix();
-        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        //GL11.glTranslatef((float)1, (float)1 + 2.0F, (float)1 + 1.0F);
-        GL11.glScalef(1.0F, -1.0F, -1.0F);
-        GL11.glTranslatef(0.5F, 0.5F, 0.5F);
-        GL11.glPopMatrix();
+		GL11.glPushMatrix();
+		GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		//GL11.glTranslatef((float)1, (float)1 + 2.0F, (float)1 + 1.0F);
+		GL11.glScalef(1.0F, -1.0F, -1.0F);
+		GL11.glTranslatef(0.5F, 0.5F, 0.5F);
+		GL11.glPopMatrix();
 		Tessellator v5 = new Tessellator();
 		if (type == type.INVENTORY) {
 			if (v5.isDrawing)
@@ -72,9 +55,9 @@ public final class ReikaSpriteSheets {
 			v5.addVertexWithUV(0, 1, 0, 0.0625F*col, 0.0625F*row);
 			v5.draw();
 		}
-		if (type == type.EQUIPPED || type == type.ENTITY) {
+		if (type == type.EQUIPPED || type == type.EQUIPPED_FIRST_PERSON || type == type.ENTITY) {
 			//Args: x-hi, z-lo, x-lo, z-hi, sheet width, sheet height, thickness
-			if (type == type.EQUIPPED) {
+			if (type == type.EQUIPPED || type == type.EQUIPPED_FIRST_PERSON) {
 				GL11.glTranslatef(0, 1.25F, 0.3125F);
 				GL11.glRotatef(60, 0, 1, 0);
 				GL11.glRotatef(65, 0, 0, 1);
@@ -85,6 +68,18 @@ public final class ReikaSpriteSheets {
 				GL11.glTranslatef(0.5F, 0, 0.25F);
 				GL11.glRotatef(-10, 0, 1, 0);
 				GL11.glTranslatef(-0.125F, -0.125F, 0F);
+
+				/*
+				 * 
+				GL11.glRotatef(90, 1, 0, 1);
+				GL11.glRotatef(90, 0, 1, 0);
+				GL11.glRotatef(-90, 0, 0, 1);
+				GL11.glTranslated(-0.75, 0, 0);
+				GL11.glTranslated(0, -1, 0);
+				GL11.glTranslated(0, 0, 1);
+				GL11.glScaled(1.5, 1.5, 1.5);
+				 * 
+				 */
 			}
 			else {
 				GL11.glTranslatef(-0.5F, 0, 0);
@@ -92,8 +87,8 @@ public final class ReikaSpriteSheets {
 			float thick = 0.0625F;
 			ItemRenderer.renderItemIn2D(v5, 0.0625F+0.0625F*col, 0.0625F*row, 0.0625F*col, 0.0625F+0.0625F*row, 256, 256, thick);
 		}
-        GL11.glEnable(GL11.GL_LIGHTING);
+		GL11.glEnable(GL11.GL_LIGHTING);
 		//ModLoader.getMinecraftInstance().entityRenderer.enableLightmap(1);
 	}
-	
+
 }
