fb968a027c5a4fcdcbfb4283a536bb45be8aa8c6
Reika <reikasminecraft@gmail.com>
2013-08-13 23:29:52 -0400

    Update

--- Instantiable/ObjectWeb.java
+++ Instantiable/ObjectWeb.java
@@ -9,6 +9,7 @@
  ******************************************************************************/
 package Reika.DragonAPI.Instantiable;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -16,7 +17,17 @@ import Reika.DragonAPI.Libraries.ReikaJavaLibrary;
 
 public class ObjectWeb {
 
-	private HashMap<Object, List> web = new HashMap<Object, List>();
+	private final HashMap<Object, List> web = new HashMap<Object, List>();
+
+	private final Class nodeClass;
+
+	public ObjectWeb() {
+		nodeClass = null;
+	}
+
+	public ObjectWeb(Class node) {
+		nodeClass = node;
+	}
 
 	public boolean isDirectionallyConnectedTo(Object parent, Object child) {
 		if (!this.hasNode(parent))
@@ -39,8 +50,13 @@ public class ObjectWeb {
 	}
 
 	public void addNode(Object obj) {
+		if (nodeClass != null && nodeClass != obj.getClass()) {
+			ReikaJavaLibrary.pConsole("Node "+obj+" is an invalid class type!");
+			Thread.dumpStack();
+			return;
+		}
 		if (!this.hasNode(obj))
-			web.put(obj, null);
+			web.put(obj, new ArrayList());
 	}
 
 	public boolean hasNode(Object obj) {
@@ -48,31 +64,39 @@ public class ObjectWeb {
 	}
 
 	public void addDirectionalConnection(Object parent, Object child) {
-		if (this.hasNode(parent)) {
-			this.addChild(parent, child);
+		if (parent.getClass() != child.getClass()) {
+			ReikaJavaLibrary.pConsole("Cannot add links between incompatible class types!");
+			Thread.dumpStack();
+			return;
 		}
-		else {
-			web.put(parent, ReikaJavaLibrary.makeListFrom(child));
-		}
+		if (!this.hasNode(parent))
+			this.addNode(parent);
+		this.addChild(parent, child);
 	}
 
 	public void addBilateralConnection(Object a, Object b) {
-		if (this.hasNode(a)) {
-			this.addChild(a, b);
-		}
-		else {
-			web.put(a, ReikaJavaLibrary.makeListFrom(b));
+		if (a.getClass() != b.getClass()) {
+			ReikaJavaLibrary.pConsole("Cannot add links between incompatible class types!");
+			Thread.dumpStack();
+			return;
 		}
 
-		if (this.hasNode(b)) {
-			this.addChild(b, a);
-		}
-		else {
-			web.put(b, ReikaJavaLibrary.makeListFrom(a));
-		}
+		if (!this.hasNode(a))
+			this.addNode(a);
+		this.addChild(a, b);
+
+		if (!this.hasNode(b))
+			this.addNode(b);
+		this.addChild(b, a);
 	}
 
 	public void addChild(Object parent, Object child) {
+		if (parent.getClass() != child.getClass()) {
+			ReikaJavaLibrary.pConsole("Cannot add links between incompatible class types!");
+			Thread.dumpStack();
+			return;
+		}
+
 		if (!this.hasNode(parent)) {
 			ReikaJavaLibrary.pConsole("Cannot add a child to a nonexistent node "+parent+"!");
 			Thread.dumpStack();
@@ -85,6 +109,28 @@ public class ObjectWeb {
 			return;
 		}
 		li.add(child);
+		web.put(parent, li);
+	}
+
+	public void removeChild(Object parent, Object child) {
+		if (parent.getClass() != child.getClass()) {
+			ReikaJavaLibrary.pConsole("Incompatible object class types!");
+			Thread.dumpStack();
+			return;
+		}
+
+		if (!this.hasNode(parent)) {
+			ReikaJavaLibrary.pConsole("Cannot remove a child from a nonexistent node "+parent+"!");
+			Thread.dumpStack();
+			return;
+		}
+		List li = web.get(parent);
+		if (!li.contains(child)) {
+			ReikaJavaLibrary.pConsole("Child "+child+" does not exist for node "+parent+"! Cannot remove!");
+			Thread.dumpStack();
+			return;
+		}
+		li.remove(child);
 		web.put(parent, li);
 	}
 
