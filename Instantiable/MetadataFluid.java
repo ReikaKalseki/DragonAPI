b6c3ae4ae77bdb148e9b01e1b35d5138a32ce95e
Reika <reikasminecraft@gmail.com>
2013-11-08 08:09:07 -0500

    Update

--- Instantiable/MetadataFluid.java
+++ Instantiable/MetadataFluid.java
@@ -10,11 +10,39 @@
 package Reika.DragonAPI.Instantiable;
 
 import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
 
 public class MetadataFluid extends Fluid {
 
-	public MetadataFluid(String fluidName) {
+	private final int metas;
+
+	public MetadataFluid(String fluidName, int nmeta) {
 		super(fluidName);
+		metas = nmeta;
 	}
 
+	public int getNumberMetadatas() {
+		return metas;
+	}
+
+	/** Use this instead of manual registration! */
+	public final void register() {
+		for (int i = 0; i < metas; i++) {
+			Fluid f = new Fluid(this.getTag(i));
+			FluidRegistry.registerFluid(f);
+		}
+	}
+
+	private final String getTag(int i) {
+		return String.format("%sD%d", fluidName, i);
+	}
+
+	public final Fluid getFluid(int metadata) {
+		return FluidRegistry.getFluid(this.getTag(metadata));
+	}
+
+	@Override
+	public final boolean equals(Object o) {
+		return ((Fluid)o).getName().contains(fluidName) && o instanceof MetadataFluid;
+	}
 }
