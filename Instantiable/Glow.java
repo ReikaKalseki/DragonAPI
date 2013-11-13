4aa299fb44bba4eb726c3c9458cebad6604f1b04
Reika <reikasminecraft@gmail.com>
2013-11-13 08:04:53 -0500

    Oops

--- Instantiable/Glow.java
+++ Instantiable/Glow.java
@@ -7,7 +7,7 @@
  * Distribution of the software in any form is only allowed with
  * explicit, prior permission from the owner.
  ******************************************************************************/
-package Reika.DragonAPI.Interfaces;
+package Reika.DragonAPI.Instantiable;
 
 import org.lwjgl.opengl.GL11;
 
