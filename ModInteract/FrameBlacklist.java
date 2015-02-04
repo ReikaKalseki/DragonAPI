01d80766d64952b9173ba832d94be3b285b0305d
Reika <reikasminecraft@gmail.com>
2015-02-04 00:21:39 -0500

    Update

--- ModInteract/FrameBlacklist.java
+++ ModInteract/FrameBlacklist.java
@@ -1,10 +1,11 @@
 package Reika.DragonAPI.ModInteract;
 
-import java.util.HashSet;
-
 import net.minecraft.block.Block;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
-import Reika.DragonAPI.Instantiable.Data.BlockKey;
+import net.minecraftforge.common.MinecraftForge;
+import Reika.DragonAPI.ASM.APIStripper.Strippable;
+import Reika.DragonAPI.ASM.DependentMethodStripper.SmartStripper;
 
 import com.amadornes.framez.api.movement.BlockMovementType;
 import com.amadornes.framez.api.movement.HandlingPriority;
@@ -12,42 +13,61 @@ import com.amadornes.framez.api.movement.HandlingPriority.Priority;
 import com.amadornes.framez.api.movement.IMovementHandler;
 import com.amadornes.framez.api.movement.IMovingBlock;
 
+import cpw.mods.fml.common.eventhandler.Cancelable;
+import cpw.mods.fml.common.eventhandler.Event;
+
 public class FrameBlacklist {
 
-	private static final HashSet<BlockKey> blacklist = new HashSet();
-
-	public static void blacklistBlock(Block b) {
-		blacklistBlock(new BlockKey(b));
+	private static boolean isBlacklisted(World world, int x, int y, int z, Block b, int meta, TileEntity te) {
+		return MinecraftForge.EVENT_BUS.post(new FrameUsageEvent(world, x, y, z, b, meta, te));
 	}
 
-	public static void blacklistBlock(Block b, int meta) {
-		blacklistBlock(new BlockKey(b, meta));
-	}
-
-	private static void blacklistBlock(BlockKey bk) {
-		blacklist.add(bk);
-	}
-
+	@Strippable("com.amadornes.framez.api.movement.IMovementHandler")
 	public static class FramezHandler implements IMovementHandler {
 
 		@Override
+		@SmartStripper
 		@HandlingPriority(Priority.HIGH)
 		public boolean handleStartMoving(IMovingBlock block) {
-			return blacklist.contains(new BlockKey(block.getBlock(), block.getMetadata()));
+			return isBlacklisted(block.getWorld(), block.getX(), block.getY(), block.getZ(), block.getBlock(), block.getMetadata(), block.getTileEntity());
 		}
 
 		@Override
+		@SmartStripper
 		@HandlingPriority(Priority.HIGH)
 		public boolean handleFinishMoving(IMovingBlock block) {
-			return blacklist.contains(new BlockKey(block.getBlock(), block.getMetadata()));
+			return isBlacklisted(block.getWorld(), block.getX(), block.getY(), block.getZ(), block.getBlock(), block.getMetadata(), block.getTileEntity());
 		}
 
 		@Override
+		@SmartStripper
 		@HandlingPriority(Priority.HIGH)
 		public BlockMovementType getMovementType(World world, Integer x, Integer y, Integer z) {
-			Block b = world.getBlock(x, y, z);
-			int meta = world.getBlockMetadata(x, y, z);
-			return blacklist.contains(new BlockKey(b, meta)) ? BlockMovementType.UNMOVABLE : null;
+			return isBlacklisted(world, x, y, z, world.getBlock(x, y, z), world.getBlockMetadata(x, y, z), world.getTileEntity(x, y, z)) ? BlockMovementType.UNMOVABLE : null;
+		}
+
+	}
+
+	@Cancelable
+	public static class FrameUsageEvent extends Event {
+
+		public final World world;
+		public final int xCoord;
+		public final int yCoord;
+		public final int zCoord;
+
+		public final Block block;
+		public final int metadata;
+		public final TileEntity tile;
+
+		private FrameUsageEvent(World w, int x, int y, int z, Block b, int meta, TileEntity te) {
+			world = w;
+			xCoord = x;
+			yCoord = y;
+			zCoord = z;
+			block = b;
+			metadata = meta;
+			tile = te;
 		}
 
 	}
