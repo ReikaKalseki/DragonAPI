477f97ffd5d651212f6676de1a03c463bbf0d17d
Reika <reikasminecraft@gmail.com>
2015-02-08 14:34:27 -0500

    Update

--- DragonAPICore.java
+++ DragonAPICore.java
@@ -38,7 +38,7 @@ public class DragonAPICore {
 
 	private static boolean loaded;
 
-	private static final String MINFORGE = "required-after:Forge@[10.13.0.1205,);";
+	private static final String MINFORGE = "required-after:Forge@[10.13.0.1231,);"; //was 1205
 	public static final String dependencies = MINFORGE+"after:BuildCraft|Energy;after:IC2;after:ThermalExpansion;after:Thaumcraft;"+
 			"after:powersuits;after:GalacticCraft;after:Mystcraft;after:UniversalElectricity;after:Forestry;after:MagicBees;"+
 			"after:BinnieCore;after:Natura;after:TConstruct;after:ProjRed|Core;after:bluepower";
