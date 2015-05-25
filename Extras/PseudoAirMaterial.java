e39cd5413684cb8a4a26ea7619688d466f6dd224
Reika <reikasminecraft@gmail.com>
2015-05-24 20:26:19 -0400

    Update 713

---
+++ Extras/PseudoAirMaterial.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * @author Reika Kalseki
+ * 
+ * Copyright 2015
+ * 
+ * All rights reserved.
+ * Distribution of the software in any form is only allowed with
+ * explicit, prior permission from the owner.
+ ******************************************************************************/
+package Reika.DragonAPI.Extras;
+
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+
+public class PseudoAirMaterial extends Material {
+
+	public static final PseudoAirMaterial instance = new PseudoAirMaterial();
+
+	private PseudoAirMaterial() {
+		super(MapColor.airColor);
+
+		this.setReplaceable();
+		this.setNoPushMobility();
+	}
+
+	@Override
+	public boolean isSolid() {
+		return false;
+	}
+
+	@Override
+	public boolean blocksMovement()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean isOpaque() {
+		return false;
+	}
+
+	@Override
+	public boolean getCanBlockGrass()
+	{
+		return false;
+	}
+
+}
