b663d710f3101e03fc31fe78e2570617d62aea55
Reika <reikasminecraft@gmail.com>
2020-03-29 01:52:40 -0400

    Update 1031

--- Instantiable/IO/RemoteSourcedAsset.java
+++ Instantiable/IO/RemoteSourcedAsset.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import Reika.DragonAPI.DragonAPICore;
+import Reika.DragonAPI.Auxiliary.Trackers.RemoteAssetLoader;
+import Reika.DragonAPI.Auxiliary.Trackers.RemoteAssetLoader.RemoteAsset;
+import Reika.DragonAPI.Auxiliary.Trackers.RemoteAssetLoader.RemoteAssetRepository;
+import Reika.DragonAPI.Base.DragonAPIMod;
 
 public class RemoteSourcedAsset {
 
@@ -16,7 +20,7 @@ public class RemoteSourcedAsset {
 	public final String remotePath;
 	public final String localRemote;
 
-	public RemoteSourcedAsset(Class ref, String s, String rem, String loc) {
+	private RemoteSourcedAsset(Class ref, String s, String rem, String loc) {
 		reference = ref;
 		path = s;
 		remotePath = rem;
@@ -32,12 +36,17 @@ public class RemoteSourcedAsset {
 		if (f.exists()) {
 			return new FileInputStream(f);
 		}
-		f = new File(this.getFallbackPath());
+		String fall = this.getFallbackPath();
+		f = new File(fall);
 		if (f.exists()) {
 			return new FileInputStream(f);
 		}
 		else {
-			DragonAPICore.logError("Could not find any resource for asset "+reference+"/"+path+"!");
+			DragonAPICore.logError("Could not find main resource for asset "+reference+"/"+path+"!");
+			InputStream in = reference.getResourceAsStream(fall);
+			if (in != null)
+				return in;
+			DragonAPICore.logError("Could not find ANY resource for asset "+reference+"/"+path+"!");
 			return null;
 		}
 	}
@@ -62,6 +71,68 @@ public class RemoteSourcedAsset {
 		String ext = this.getFileExt();
 		String main = path.substring(0, path.length()-ext.length());
 		return main+"_fallback"+ext;
+	}
+
+	public static class RemoteSourcedAssetRepository {
+
+		public final Class rootClass;
+		public final String rootPath;
+		public final String rootRemote;
+		public final String rootLocal;
+
+		public final DragonAPIMod owner;
+		private final RemoteAssetRepository repository;
+
+		public RemoteSourcedAssetRepository(DragonAPIMod mod, Class c, String r, String l) {
+			this(mod, c, "", r, l);
+		}
+
+		public RemoteSourcedAssetRepository(DragonAPIMod mod, Class c, String p, String r, String l) {
+			rootClass = c;
+			rootLocal = l;
+			rootRemote = r;
+			rootPath = p;
+
+			owner = mod;
+			repository = new DynamicRemoteAssetRepository();
+		}
+
+		public RemoteSourcedAsset createAsset(String file) {
+			return new RemoteSourcedAsset(rootClass, rootPath.isEmpty() ? file : rootPath+"/"+file, rootRemote+"/"+file, rootLocal);
+		}
+
+		public void addToAssetLoader() {
+			RemoteAssetLoader.instance.registerAssets(repository);
+		}
+
+		private class DynamicRemoteAssetRepository extends RemoteAssetRepository {
+
+			private DynamicRemoteAssetRepository() {
+				super(owner);
+			}
+
+			@Override
+			public String getRepositoryURL() {
+				return rootRemote;
+			}
+
+			@Override
+			protected RemoteAsset parseAsset(String line) {
+				return new MusicAsset(this);
+			}
+
+			@Override
+			public String getDisplayName() {
+
+			}
+
+			@Override
+			public String getLocalPath() {
+				return rootLocal;
+			}
+
+		}
+
 	}
 
 }
--- Libraries/IO/ReikaTextureHelper.java
+++ Libraries/IO/ReikaTextureHelper.java
@@ -12,6 +12,7 @@ package Reika.DragonAPI.Libraries.IO;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Field;
@@ -19,6 +20,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
@@ -41,8 +44,9 @@ import Reika.DragonAPI.Exception.MisuseException;
 import Reika.DragonAPI.IO.ReikaImageLoader;
 import Reika.DragonAPI.IO.ReikaImageLoader.ImageEditor;
 import Reika.DragonAPI.IO.ReikaTextureBinder;
-import Reika.DragonAPI.Instantiable.Data.Maps.PluralMap;
+import Reika.DragonAPI.Instantiable.Data.Maps.NestedMap;
 import Reika.DragonAPI.Instantiable.Event.Client.TextureReloadEvent;
+import Reika.DragonAPI.Instantiable.IO.RemoteSourcedAsset;
 import Reika.DragonAPI.Libraries.Java.ReikaJavaLibrary;
 import Reika.DragonAPI.Libraries.Java.ReikaObfuscationHelper;
 import Reika.DragonAPI.Libraries.Java.ReikaObfuscationHelper.ReflectiveAccessExceptionHandler;
@@ -54,8 +58,8 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class ReikaTextureHelper {
 
-	/** Keys: Class, Path */
-	private static final PluralMap textures = new PluralMap(2);
+	private static final HashMap<TextureIdentifier, Integer> textures = new HashMap();
+	private static final NestedMap<Class, String, TextureIdentifier> textureKeys = new NestedMap();
 
 	private static final HashMap<ReikaDyeHelper, Integer> colorOverrides = new HashMap();
 	private static boolean noColorPacks = false;
@@ -89,11 +93,38 @@ public class ReikaTextureHelper {
 	}
 
 	public static void resetTexture(Class c, String tex) {
-		textures.remove(c, tex);
+		textures.remove(createTextureID(c, tex));
 	}
 
 	public static int bindTexture(Class root, String tex) {
 		return bindTexture(root, tex, null);
+	}
+
+	public static int bindTexture(RemoteSourcedAsset rem) {
+		if (reload()) {
+			textures.clear();
+			colorOverrides.clear();
+			MinecraftForge.EVENT_BUS.post(new TextureReloadEvent());
+			return 0;
+		}
+		else {
+			TextureIdentifier id = createTextureID(rem);
+			Integer gl = textures.get(id);
+			if (gl == null) {
+				DragonAPICore.log("Loading dynamically sourced asset "+rem.path);
+				try {
+					gl = bindRawDataTexture(rem.getData());
+				}
+				catch (IOException e) {
+					DragonAPICore.logError("Could not load asset!");
+					e.printStackTrace();
+					gl = binder.allocateAndSetupTexture(ReikaImageLoader.getMissingTex());
+				}
+				textures.put(id, gl);
+			}
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, gl.intValue());
+			return gl.intValue();
+		}
 	}
 
 	public static int bindTexture(Class root, String tex, ImageEditor img) {
@@ -115,14 +146,15 @@ public class ReikaTextureHelper {
 				tex = tex.substring(1);
 			String respath = tex.startsWith(parent) ? tex : parent+tex;
 
-			Integer gl = (Integer)textures.get(root, tex);
+			TextureIdentifier id = createTextureID(root, tex);
+			Integer gl = textures.get(id);
 			if (gl == null) {
 				ArrayList<IResourcePack> li = getCurrentResourcePacks();
 				Collections.reverse(li); //because Mojang
 				for (IResourcePack res : li) {
 					gl = bindPackTexture(root, respath, res, img);
 					if (gl != null) {
-						textures.put(gl, root, tex);
+						textures.put(id, gl);
 						DragonAPICore.log("Texture Pack "+res.getPackName()+" contains an image for "+tex+".");
 						break;
 					}
@@ -131,7 +163,7 @@ public class ReikaTextureHelper {
 			if (gl == null) {
 				DragonAPICore.log("No texture packs contain an image for "+tex+". Loading default.");
 				gl = bindClassReferencedTexture(root, oldtex, img);
-				textures.put(gl, root, tex);
+				textures.put(id, gl);
 			}
 			GL11.glBindTexture(GL11.GL_TEXTURE_2D, gl.intValue());
 			return gl.intValue();
@@ -140,13 +172,29 @@ public class ReikaTextureHelper {
 
 	/** To disallow resource packs to change it */
 	public static int bindFinalTexture(Class root, String tex) {
-		Integer gl = (Integer)textures.get(root, tex);
+		TextureIdentifier id = createTextureID(root, tex);
+		Integer gl = textures.get(id);
 		if (gl == null) {
 			gl = bindClassReferencedTexture(root, tex, null);
-			textures.put(gl, root, tex);
+			textures.put(id, gl);
 		}
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, gl.intValue());
 		return gl.intValue();
+	}
+
+	private static Integer bindRawDataTexture(InputStream in) throws IOException {
+		if (in == null) {
+			DragonAPICore.logError("No image found!");
+			return new Integer(binder.allocateAndSetupTexture(ReikaImageLoader.getMissingTex()));
+		}
+		BufferedImage img = ImageIO.read(in);
+		if (img == null) {
+			DragonAPICore.logError("No image found!");
+			return new Integer(binder.allocateAndSetupTexture(ReikaImageLoader.getMissingTex()));
+		}
+		else {
+			return new Integer(binder.allocateAndSetupTexture(img));
+		}
 	}
 
 	private static Integer bindClassReferencedTexture(Class root, String tex, ImageEditor editor) {
@@ -161,17 +209,18 @@ public class ReikaTextureHelper {
 	}
 
 	public static int bindRawTexture(String tex) {
-		Integer gl = (Integer)textures.get(null, tex);
+		TextureIdentifier id = createTextureID(tex);
+		Integer gl = textures.get(id);
 		if (gl == null) {
 			BufferedImage img = ReikaImageLoader.readHardPathImage(tex);
 			if (img == null) {
 				DragonAPICore.logError("No image found for "+tex+"!");
 				gl = new Integer(binder.allocateAndSetupTexture(ReikaImageLoader.getMissingTex()));
-				textures.put(gl, null, tex);
+				textures.put(id, gl);
 			}
 			else {
 				gl = new Integer(binder.allocateAndSetupTexture(img));
-				textures.put(gl, null, tex);
+				textures.put(id, gl);
 			}
 		}
 		if (gl != null)
@@ -179,11 +228,12 @@ public class ReikaTextureHelper {
 		return gl != null ? gl.intValue() : 0;
 	}
 
-	public static int bindRawTexture(BufferedImage tex, String id) {
-		Integer gl = (Integer)textures.get(null, id);
+	public static int bindRawTexture(BufferedImage tex, String ids) {
+		TextureIdentifier id = createTextureID(ids);
+		Integer gl = textures.get(id);
 		if (gl == null) {
 			gl = new Integer(binder.allocateAndSetupTexture(tex));
-			textures.put(gl, null, id);
+			textures.put(id, gl);
 		}
 		if (gl != null)
 			GL11.glBindTexture(GL11.GL_TEXTURE_2D, gl.intValue());
@@ -193,6 +243,28 @@ public class ReikaTextureHelper {
 	private static Integer bindPackTexture(Class root, String tex, IResourcePack res, ImageEditor editor) {
 		BufferedImage img = ReikaImageLoader.getImageFromResourcePack(tex, res, editor);
 		return img != null ? new Integer(binder.allocateAndSetupTexture(img)) : null;
+	}
+
+	private static TextureIdentifier createTextureID(Class c, String tex) {
+		TextureIdentifier ref = textureKeys.get(c, tex);
+		if (ref == null) {
+			ref = new ClassRefTexture(c, tex);
+			textureKeys.put(c, tex, ref);
+		}
+		return ref;
+	}
+
+	private static TextureIdentifier createTextureID(String tex) {
+		TextureIdentifier ref = textureKeys.get(null, tex);
+		if (ref == null) {
+			ref = new PathTexture(tex);
+			textureKeys.put(null, tex, ref);
+		}
+		return ref;
+	}
+
+	private static TextureIdentifier createTextureID(RemoteSourcedAsset rem) {
+		return createTextureID(rem.reference, rem.path);
 	}
 
 	public static void bindTerrainTexture() {
@@ -368,6 +440,61 @@ public class ReikaTextureHelper {
 
 		protected GenSprite(String s) {
 			super(s);
+		}
+
+	}
+
+	private static class PathTexture extends TextureIdentifier {
+
+		private final String path;
+
+		private PathTexture(String p) {
+			path = p;
+		}
+
+		@Override
+		public int hashCode() {
+			return path.hashCode();
+		}
+
+		@Override
+		public boolean equals(TextureIdentifier o) {
+			return o instanceof PathTexture && ((PathTexture)o).path.equals(path);
+		}
+
+	}
+
+	private static class ClassRefTexture extends TextureIdentifier {
+
+		private final Class ref;
+		private final String path;
+
+		private ClassRefTexture(Class c, String p) {
+			ref = c;
+			path = p;
+		}
+
+		@Override
+		public int hashCode() {
+			return ref.hashCode() ^ path.hashCode();
+		}
+
+		@Override
+		public boolean equals(TextureIdentifier o) {
+			return o instanceof ClassRefTexture && ((ClassRefTexture)o).ref == ref && ((ClassRefTexture)o).path.equals(path);
+		}
+
+	}
+
+	private static abstract class TextureIdentifier {
+
+		@Override
+		public abstract int hashCode();
+		public abstract boolean equals(TextureIdentifier o);
+
+		@Override
+		public final boolean equals(Object o) {
+			return o instanceof TextureIdentifier && this.equals(o);
 		}
 
 	}
