9b55c8b65c144c11f889305721492f33fa729a58
Reika <reikasminecraft@gmail.com>
2021-02-07 18:52:03 -0500

    Update 1008

--- Instantiable/MusicScore.java
+++ Instantiable/MusicScore.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * @author Reika Kalseki
- * 
+ *
  * Copyright 2017
- * 
+ *
  * All rights reserved.
  * Distribution of the software in any form is only allowed with
  * explicit, prior permission from the owner.
@@ -15,6 +15,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 import net.minecraft.nbt.NBTTagCompound;
@@ -29,7 +30,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class MusicScore {
 
-	private final TreeMap<Integer, Collection<Note>>[] music;
+	private final TreeMap<Integer, NoteData>[] music;
 	private final int channelCount;
 	private int length;
 
@@ -46,9 +47,9 @@ public class MusicScore {
 		if (music[channel] == null) {
 			music[channel] = new TreeMap();
 		}
-		Collection<Note> c = music[channel].get(time);
+		NoteData c = music[channel].get(time);
 		if (c == null) {
-			c = new ArrayList();
+			c = new NoteData(time);
 			music[channel].put(time, c);
 		}
 		else {
@@ -58,20 +59,20 @@ public class MusicScore {
 		length = Math.max(length, time);
 	}
 
-	public ArrayList<Note> getNotes(int time) {
-		ArrayList<Note> li = new ArrayList();
+	public Collection<Note> getNotes(int time) {
+		Collection<Note> li = new ArrayList();
 		for (int i = 0; i < channelCount; i++) {
-			Collection<Note> n = music[i] != null ? music[i].get(time) : null;
+			NoteData n = music[i] != null ? music[i].get(time) : null;
 			if (n != null) {
-				li.addAll(n);
+				li.addAll(n.notes.values());
 			}
 		}
 		return li;
 	}
 
 	public Collection<Note> getNotes(int channel, int time) {
-		Collection<Note> c = music[channel] != null ? music[channel].get(time) : null;
-		return c != null ? Collections.unmodifiableCollection(c) : null;
+		NoteData c = music[channel] != null ? music[channel].get(time) : null;
+		return c != null ? Collections.unmodifiableCollection(c.notes.values()) : null;
 	}
 
 	public void backspace(int channel) {
@@ -79,7 +80,7 @@ public class MusicScore {
 			music[channel].remove(music[channel].lastKey());
 	}
 
-	public Map<Integer, Collection<Note>> getTrack(int channel) {
+	public Map<Integer, NoteData> getTrack(int channel) {
 		return music[channel] != null ? Collections.unmodifiableMap(music[channel]) : new HashMap();
 	}
 
@@ -101,15 +102,119 @@ public class MusicScore {
 		for (int i = 0; i < channelCount; i++) {
 			if (music[i] != null) {
 				for (int time : music[i].keySet()) {
-					Collection<Note> c = music[i].get(time);
+					NoteData c = music[i].get(time);
 					if (c != null)
-						for (Note n : c)
+						for (Note n : c.notes.values())
 							mus.addNote(i, (int)(time/factor), n);
 				}
 			}
 		}
 
 		return mus;
+	}
+
+	public void writeToNBT(NBTTagCompound tag) {
+		for (int i = 0; i < channelCount; i++) {
+			NBTTagCompound nbt = new NBTTagCompound();
+			if (music[i] != null) {
+				for (int time : music[i].keySet()) {
+					NoteData c = music[i].get(time);
+					NBTTagList li = c.writeToNBT();
+					nbt.setTag(String.valueOf(time), li);
+				}
+			}
+			tag.setTag("Ch_"+i, nbt);
+		}
+		tag.setInteger("numchan", channelCount);
+	}
+
+	public static MusicScore readFromNBT(NBTTagCompound tag) {
+		MusicScore mus = new MusicScore(tag.getInteger("numchan"));
+
+		for (int i = 0; i < mus.channelCount; i++) {
+			if (tag.hasKey("Ch_"+i)) {
+				mus.music[i] = new TreeMap();
+				NBTTagCompound nbt = tag.getCompoundTag("Ch_"+i);
+				for (Object o : nbt.func_150296_c()) {
+					String s = (String)o;
+					int time = Integer.parseInt(s);
+					NBTTagList li = nbt.getTagList(s, NBTTypes.COMPOUND.ID);
+					NoteData c = NoteData.readFromNBT(time, li);
+					mus.music[i].put(time, c);
+				}
+			}
+		}
+
+		return mus;
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.toString(music);
+	}
+
+	public void clearChannel(int channel) {
+		music[channel] = null;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void renderPianoRoll() {
+
+	}
+
+	public MusicScore copy() {
+		MusicScore mus = new MusicScore(channelCount);
+		for (int i = 0; i < channelCount; i++) {
+			if (music[i] != null) {
+				mus.music[i] = new TreeMap();
+				mus.music[i].putAll(music[i]);
+			}
+		}
+		mus.length = length;
+		return mus;
+	}
+
+	public static class NoteData {
+
+		private final HashMap<MusicKey, Note> notes = new HashMap();
+		private final long tick;
+
+		private NoteData(long t) {
+			tick = t;
+		}
+
+		private static NoteData readFromNBT(long t, NBTTagList li) {
+			NoteData dat = new NoteData(t);
+			for (Object o2 : li.tagList) {
+				NBTTagCompound val = (NBTTagCompound)o2;
+				Note n = Note.readFromNBT(val);
+				dat.add(n);
+			}
+			return dat;
+		}
+
+		private NBTTagList writeToNBT() {
+			NBTTagList li = new NBTTagList();
+			for (Note n : notes.values()) {
+				NBTTagCompound val = new NBTTagCompound();
+				n.writeToNBT(val);
+				li.appendTag(val);
+			}
+			return li;
+		}
+
+		private void add(Note note) {
+			notes.put(note.key, note);
+		}
+
+		public Collection<Note> notes() {
+			return Collections.unmodifiableCollection(notes.values());
+		}
+
+		public Set<MusicKey> keys() {
+			return Collections.unmodifiableSet(notes.keySet());
+		}
+
 	}
 
 	public static class Note {
@@ -166,77 +271,6 @@ public class MusicScore {
 			return false;
 		}
 
-	}
-
-	public void writeToNBT(NBTTagCompound tag) {
-		for (int i = 0; i < channelCount; i++) {
-			NBTTagCompound nbt = new NBTTagCompound();
-			if (music[i] != null) {
-				for (int time : music[i].keySet()) {
-					Collection<Note> c = music[i].get(time);
-					NBTTagList li = new NBTTagList();
-					for (Note n : c) {
-						NBTTagCompound val = new NBTTagCompound();
-						n.writeToNBT(val);
-						li.appendTag(val);
-					}
-					nbt.setTag(String.valueOf(time), li);
-				}
-			}
-			tag.setTag("Ch_"+i, nbt);
-		}
-		tag.setInteger("numchan", channelCount);
-	}
-
-	public static MusicScore readFromNBT(NBTTagCompound tag) {
-		MusicScore mus = new MusicScore(tag.getInteger("numchan"));
-
-		for (int i = 0; i < mus.channelCount; i++) {
-			if (tag.hasKey("Ch_"+i)) {
-				mus.music[i] = new TreeMap();
-				NBTTagCompound nbt = tag.getCompoundTag("Ch_"+i);
-				for (Object o : nbt.func_150296_c()) {
-					String s = (String)o;
-					int time = Integer.parseInt(s);
-					NBTTagList li = nbt.getTagList(s, NBTTypes.COMPOUND.ID);
-					Collection<Note> c = new ArrayList();
-					for (Object o2 : li.tagList) {
-						NBTTagCompound val = (NBTTagCompound)o2;
-						Note n = Note.readFromNBT(val);
-						c.add(n);
-					}
-					mus.music[i].put(time, c);
-				}
-			}
-		}
-
-		return mus;
-	}
-
-	@Override
-	public String toString() {
-		return Arrays.toString(music);
-	}
-
-	public void clearChannel(int channel) {
-		music[channel] = null;
-	}
-
-	@SideOnly(Side.CLIENT)
-	public void renderPianoRoll() {
-
-	}
-
-	public MusicScore copy() {
-		MusicScore mus = new MusicScore(channelCount);
-		for (int i = 0; i < channelCount; i++) {
-			if (music[i] != null) {
-				mus.music[i] = new TreeMap();
-				mus.music[i].putAll(music[i]);
-			}
-		}
-		mus.length = length;
-		return mus;
 	}
 
 }
